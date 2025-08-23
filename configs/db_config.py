09bd0c93a4f46c6ab1f21e6089533ca6cb225638
YOOJIIN <yoojiin0328@naver.com>
2025-08-23 16:52:01 +0900

    chore(config): DB 접속 정보 이름 변경

--- configs/db_config.py
+++ configs/db_config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 # DB 설정 불러오기
 DB_CONFIG = {
-    'host': os.getenv("DB_HOST"),
+    'host': os.getenv("DB_SERVER"),
     'port': int(os.getenv("DB_PORT", 1433)),  # 기본값 1433
     'user': os.getenv("DB_USER"),
     'password': os.getenv("DB_PASSWORD"),
